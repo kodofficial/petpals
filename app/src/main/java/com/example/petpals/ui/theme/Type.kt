ac47c0b6dfca34b967f14ef65c9e9f523dcdd36e
eftseliou <ef.tseliou@gmail.com>
2024-12-09 12:55:38 +0200

    διορθωση στις ονομασιες του Type

--- app/src/main/java/com/example/petpals/ui/theme/Type.kt
+++ app/src/main/java/com/example/petpals/ui/theme/Type.kt
@@ -1,11 +1,10 @@
 package com.example.petpals.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.graphics.Color
 
 val Typography = Typography(
     titleLarge = TextStyle(
