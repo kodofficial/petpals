d25874664e76e1a41c37147db70a35cdc02c90be
Dimitrios Kontis <t8210224@aueb.gr>
2024-12-15 23:47:44 +0200

    Make system bars transparent
    plus change background color

--- app/src/main/java/com/example/petpals/ui/theme/Color.kt
+++ app/src/main/java/com/example/petpals/ui/theme/Color.kt
@@ -4,5 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val PrimaryColor = Color(0xFF2623BF)
 val SecondaryColor = Color(0xFF6764D2)
-val BackgroundColor = Color(0xFFFFFFFF)
+val BackgroundColor = Color(0xFFF8F9FC)
 val TextColor = Color(0xFF000000)
