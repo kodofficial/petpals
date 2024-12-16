6136f06484daa28840e3d8801da2b2498698f03d
Dimitrios Kontis <t8210224@aueb.gr>
2024-12-16 03:38:37 +0200

    UI additions and fixes
    1. Add logo, search icon>
    2. Make adjustments to Type.kt and add global size for icons.

--- app/src/main/java/com/example/petpals/ui/theme/Type.kt
+++ app/src/main/java/com/example/petpals/ui/theme/Type.kt
@@ -10,19 +10,33 @@ import androidx.compose.ui.graphics.Color
 val Typography = Typography(
     titleLarge = TextStyle(
         fontWeight = FontWeight.Bold,
-        fontSize = 30.sp,
-        color = PrimaryColor
+        fontSize = 32.sp,
+        color = TextColor
     ),
     titleMedium = TextStyle(
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
+        fontSize = 20.sp,
+        color = TextColor // Για τα ονόματα των sections ανά screen
+    ),
+    titleSmall = TextStyle(
+        fontSize = 12.sp,
+        color = Color.Gray // Για τον υπότιτλο στο homescreen
+    ),
+
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
         color = TextColor // Για τα ονόματα των ζώων στις αγγελιες
     ),
-    bodyLarge = TextStyle(
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        color = TextColor // Απλό κείμενο
+    bodyMedium = TextStyle(
+        fontSize = 13.sp,
+        color = TextColor // Για τα περιεχόμενα των ζώων στις αγγελιες
     ),
+    bodySmall = TextStyle(
+        fontSize = 10.sp,
+        color = TextColor // Για τον υπότιτλο στα ζώα στις αγγελιες
+    ),
+
     labelLarge = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
