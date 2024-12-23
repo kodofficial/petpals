965bd1262888a3f80aa8d57da665789098f6a2d8
kodofficial <jimiskon2003@gmail.com>
2024-12-23 14:05:09 +0200

    Add Pet data class

---
+++ app/src/main/java/com/example/petpals/data/Pet.kt
@@ -0,0 +1,14 @@
+package com.example.petpals.data;
+
+data class Pet(
+       var id: Int,
+       val name: String,
+       val species: String,
+       val breed: String?,
+       val age: Int?,
+       val gender: String,
+       val description: String?,
+       val imageUrl: String?,
+       val uploadDate: Long,
+       val location: String?
+)
