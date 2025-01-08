017df70e6b37a8e9059406229afc030cdfc6f9a4
eftseliou <ef.tseliou@gmail.com>
2025-01-08 21:26:49 +0200

    αφαιρεση keyboard options x2

--- app/src/main/java/com/example/petpals/pages/ProfilePage.kt
+++ app/src/main/java/com/example/petpals/pages/ProfilePage.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.petpals.data.User
 import com.example.petpals.data.Pet
