9926c35f87fadd6c5d69b2439572f6e921e22394
Dimitrios Kontis <t8210224@aueb.gr>
2025-01-16 01:15:50 +0200

    Update PetsGrid.kt

--- app/src/main/java/com/example/petpals/ui/theme/PetsGrid.kt
+++ app/src/main/java/com/example/petpals/ui/theme/PetsGrid.kt
@@ -1,17 +1,13 @@
 package com.example.petpals.ui.theme
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
